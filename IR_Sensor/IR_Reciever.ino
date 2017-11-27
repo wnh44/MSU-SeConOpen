d6ec7e3c28bea2a588026d19db1326667580f271
SciGuy75 <32376802+SciGuy75@users.noreply.github.com>
2017-11-27 17:13:27 -0600

    Add files via upload

---
+++ IR_Sensor/IR_Reciever.ino
@@ -0,0 +1,23 @@
+#include <IRLibAll.h>
+
+IRrecvPCI myReciever(12);
+
+void setup() {
+  Serial.begin(9600);
+  myReciever.enableIRIn();
+  //myReciever.blink13(true);
+}
+
+void loop() {
+  if (myReciever.getResults())
+  {
+    Serial.println(recvGlobal.recvLength, DEC);
+    for (int i = 1; i < recvGlobal.recvLength; i++)
+    {
+      Serial.print(recvGlobal.recvBuffer[i], DEC);
+      Serial.print(", ");
+    }
+    Serial.println();
+    myReciever.enableIRIn();
+  }
+}
